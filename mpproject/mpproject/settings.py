8d00a42b2c31d82c4714aa107c07a73b73d0ce70
Liso <yuechen1989@gmail.com>
2015-07-19 13:09:24 -0700

    switch to sqlite for testing

--- mpproject/mpproject/settings.py
+++ mpproject/mpproject/settings.py
@@ -105,11 +105,8 @@ WSGI_APPLICATION = 'mpproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': 'test',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
     }
 }
 

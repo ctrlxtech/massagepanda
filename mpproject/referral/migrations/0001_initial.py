12a0aaa187ab3b3729f90101852b17a4e57435de
Ubuntu <ubuntu@ip-172-31-9-16.us-west-1.compute.internal>
2015-09-26 03:28:55 +0000

    try with lingering migrations

--- mpproject/referral/migrations/0001_initial.py
+++ mpproject/referral/migrations/0001_initial.py
@@ -7,8 +7,8 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('customers', '0005_auto_20150607_1223'),
-        ('payment', '0007_auto_20150525_2143'),
+        ('customers', '0001_initial'),
+        ('payment', '0001_initial'),
     ]
 
     operations = [
@@ -26,8 +26,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('status', models.CharField(max_length=1, choices=[(b'P', b'Pending'), (b'S', b'Success'), (b'C', b'Cancelled')])),
                 ('code', models.ForeignKey(to='referral.CustomerReferralCode')),
-                ('order', models.ForeignKey(to='payment.Order')),
-                ('referral', models.ForeignKey(to='customers.Customer')),
+                ('order', models.OneToOneField(to='payment.Order')),
             ],
         ),
     ]
--- mpproject/services/migrations/0001_initial.py
+++ mpproject/services/migrations/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import django.core.validators
+import uuid
 
 
 class Migration(migrations.Migration):
@@ -14,11 +15,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Service',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.UUIDField(default=uuid.uuid1, serialize=False, editable=False, primary_key=True)),
                 ('service_type', models.CharField(max_length=200)),
                 ('service_detail', models.CharField(max_length=200)),
-                ('service_time', models.FloatField(max_length=20, validators=[django.core.validators.MinValueValidator(0.0)])),
-                ('service_fee', models.FloatField(max_length=40, validators=[django.core.validators.MinValueValidator(0.0)])),
+                ('service_time', models.FloatField(validators=[django.core.validators.MinValueValidator(0.0)])),
+                ('service_sale', models.FloatField(blank=True, null=True, validators=[django.core.validators.MinValueValidator(0.0)])),
+                ('service_fee', models.FloatField(validators=[django.core.validators.MinValueValidator(0.0)])),
+                ('popularity', models.IntegerField()),
             ],
         ),
         migrations.CreateModel(

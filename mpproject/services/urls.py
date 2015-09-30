7ed239f048fff5720a05e3e09283243c56c63f81
Ubuntu <ubuntu@ip-172-31-9-16.us-west-1.compute.internal>
2015-09-30 03:58:43 +0000

    restore urls

--- mpproject/services/urls.py
+++ mpproject/services/urls.py
@@ -14,4 +14,13 @@ urlpatterns = [
     url(r'^placeOrder$', views.placeOrderFromJson, name='placeOrderFromJson'),
     url(r'^placeOrderFromPost$', views.placeOrderFromPost, name='placeOrderFromPost'),
 
+    url(r'^In-Home_Swedish_Massage_for_1_Hour$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Swedish Massage", service_time=1)}), name='swedishOneHourDetail'),
+    url(r'^In-Home_Swedish_Massage_for_1.5_Hours$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Swedish Massage", service_time=1.5)})),
+    url(r'^In-Home_Deep_Tissue_Massage_for_1_Hour$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Deep Tissue Massage", service_time=1)}), name='deepTissueOneHourDetail'),
+    url(r'^In-Home_Deep_Tissue_Massage_for_1.5_Hours$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Deep Tissue Massage", service_time=1.5)})),
+    url(r'^In-Home_Couples_Massage_for_1_Hour$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Couples Massage", service_time=1), 'couple': True})),
+    url(r'^In-Home_Couples_Massage_for_1.5_Hours$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Couples Massage", service_time=1.5), 'couple': True})),
+    url(r'^In-Home_Sports_Massage_for_1_Hour$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Sports Massage", service_time=1)})),
+    url(r'^In-Home_Sports_Massage_for_1.5_Hours$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Sports Massage", service_time=1.5)})),
+    url(r'^In-Home_Shiatsu_Massage_for_1_Hour$', DetailsView.as_view(context={'service': Service.objects.get(service_type="Shiatsu Massage", service_time=1)})),
 ]

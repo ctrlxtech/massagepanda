8eb1c1f5b6910aafacd704b61339ca1a1d917b86
Liso <yuechen1989@gmail.com>
2015-11-11 23:02:46 -0800

    reformat email

--- static/js/details.js
+++ static/js/details.js
@@ -94,8 +94,8 @@ var pandaUtil = {
     //private function
 function setupTimeDropdown($timeList) {
     $timeList.empty();
-    var timeArray = ['9:00am', '9:30am', '10:00am', '10:30am', '11:00am', '11:30am', '12:00pm', '12:30pm', '1:00pm', '1:30pm', '2:00pm', '2:30pm', '3:00pm', '3:30pm', '4:00pm', '4:30pm', '5:00pm', '5:30pm', '6:00pm', '6:30pm', '7:00pm', '7:30pm', '8:00pm', '8:30pm', '9:00pm'];
-    for (var i = 0; i < 25; i++) {
+    var timeArray = ['9:00am', '9:30am', '10:00am', '10:30am', '11:00am', '11:30am', '12:00pm', '12:30pm', '1:00pm', '1:30pm', '2:00pm', '2:30pm', '3:00pm', '3:30pm', '4:00pm', '4:30pm', '5:00pm', '5:30pm', '6:00pm', '6:30pm', '7:00pm', '7:30pm', '8:00pm', '8:30pm', '9:00pm', '9:30pm', '10:00pm'];
+    for (var i = 0; i < 27; i++) {
         var time = timeArray[i];
         var timeTempString = '<li class="mp-item"><a>' + time + '</a></li><li class="divider"></li>';
         $timeList.append(timeTempString);
@@ -119,7 +119,7 @@ function massageDetailsTimeForList() {
         var today = new Date();
         var todayDateString = pandaUtil.getDateString(today);
         if (todayDateString === date) {
-            var timeArray = ['9:00am', '9:30am', '10:00am', '10:30am', '11:00am', '11:30am', '12:00pm', '12:30pm', '1:00pm', '1:30pm', '2:00pm', '2:30pm', '3:00pm', '3:30pm', '4:00pm', '4:30pm', '5:00pm', '5:30pm', '6:00pm', '6:30pm', '7:00pm', '7:30pm', '8:00pm', '8:30pm', '9:00pm'];
+            var timeArray = ['9:00am', '9:30am', '10:00am', '10:30am', '11:00am', '11:30am', '12:00pm', '12:30pm', '1:00pm', '1:30pm', '2:00pm', '2:30pm', '3:00pm', '3:30pm', '4:00pm', '4:30pm', '5:00pm', '5:30pm', '6:00pm', '6:30pm', '7:00pm', '7:30pm', '8:00pm', '8:30pm', '9:00pm', '9:30pm', '10:00pm'];
             var currentHours = today.getHours() + 1;
             var currentMins = today.getMinutes();
             currentMins = Math.ceil(currentMins / 30) * 30;
@@ -128,7 +128,7 @@ function massageDetailsTimeForList() {
                 currentHours = currentHours + 1;
             }
             //special case if over 10,then no service
-            if (currentHours > 22) {
+            if (currentHours > 23) {
                 $timeList.find('li').not('.noServiceAvaliable').addClass('timeListHide');
                 $timeList.find('li.noServiceAvaliable').show();
                 return;
